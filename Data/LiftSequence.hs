2ab000be1ddc49a2ec36afaaa58485df56aef654
Atze van der Ploeg <atzeus@gmail.com>
2016-03-08 16:27:57 +0100

    Any primitive.

--- Data/LiftSequence.hs
+++ Data/LiftSequence.hs
@@ -1,39 +1,33 @@
-{-# LANGUAGE ScopedTypeVariables,KindSignatures,GADTs, CPP #-}
+{-# LANGUAGE ScopedTypeVariables, MagicHash,KindSignatures,GADTs, PolyKinds, CPP #-}
 module Data.LiftSequence(module Data.Interface.TSequence, LiftSeq) where
 
 -- Author : Atze van der Ploeg
 -- Lifts any sequence to a type-aligned sequence by coercion
--- Only safe iff the sequence does what we expect and doesn't 
+-- Only safe iff the sequence does what we expect and doesn't
 -- reorganize the ordering (for example due to a bug..)
 -- Use at your own risk!
 
 import Data.Interface.TSequence
 import Data.Interface.Sequence
-import Unsafe.Coerce
+import GHC.Prim
 
-data Any c where Any :: c a b -> Any c
-
-newtype LiftSeq s c a b = LiftSeq (s (Any c))
+newtype LiftSeq s (c :: k -> k -> *) (a :: k) (b :: k) = LiftSeq (s (Any c))
 
 instance Sequence s => TSequence (LiftSeq s) where
   tempty = LiftSeq empty
 
-  tsingleton x = LiftSeq (singleton (Any x))
+  tsingleton x = LiftSeq (singleton (unsafeCoerce# x))
 
-  (LiftSeq l) |> x = LiftSeq (l .|> Any x)
+  (LiftSeq l) |> x = LiftSeq (l .|> unsafeCoerce# x)
 
-  x <| (LiftSeq r) = LiftSeq (Any x .<| r)
+  x <| (LiftSeq r) = LiftSeq (unsafeCoerce# x .<| r)
 
   (LiftSeq l) >< (LiftSeq r) = LiftSeq (l .>< r)
 
   tviewl (LiftSeq l) = case viewl l of
-    EmptyL -> unsafeCoerce TEmptyL
-    h :< t -> unsafeCoerce h :| LiftSeq t
+    EmptyL -> unsafeCoerce# TEmptyL
+    h :< t -> unsafeCoerce# h :| LiftSeq t
 
   tviewr (LiftSeq l) = case viewr l of
-    EmptyR -> unsafeCoerce TEmptyR
-    p :> l -> LiftSeq p :|< unsafeCoerce l
-  
-  
-
-
+    EmptyR -> unsafeCoerce# TEmptyR
+    p :> l -> LiftSeq p :|< unsafeCoerce# l
